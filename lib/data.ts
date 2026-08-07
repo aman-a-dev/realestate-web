67c7fac97f8c1d38b47cc3f04d0d0a852f8744bf
aman-a-dev <amanuelantenha@gmail.com>
2026-08-07 12:32:37 +0300

    Yoteck real estate

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 // brand
 export const BRAND = {
-  name: "Crown Properties PLC",
+  name: "Yotek Properties",
   tagline: "Where Vision Meets Address",
   founded: 2015,
   location: "Ethiopia Addis Ababa",
