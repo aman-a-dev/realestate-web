ec25ff262f401dc80cee893f11432b9142bdad85
aman-a-dev <amanuelantenha@gmail.com>
2026-08-18 17:50:36 +0300

    Pick properties

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 // brand
 export const BRAND = {
-  name: "HomeLink Real Estate",
+  name: "Pick Properties",
   tagline: "Where Vision Meets Address",
   founded: 2015,
   location: "Ethiopia Addis Ababa",
